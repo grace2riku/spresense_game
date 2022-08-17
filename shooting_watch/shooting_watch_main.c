902a1b30f426a8f57e8253f21fee84feb08880ed
Koji Abe <gracehime@gmail.com>
2022-08-17 23:10:56 +0900

    【シュウォッチ】 ゲーム開始前のカウンタダウン表示がみずらかったので改行なしに修正した。

--- shooting_watch/shooting_watch_main.c
+++ shooting_watch/shooting_watch_main.c
@@ -9,6 +9,8 @@
 #define USER_LED_1  (46)
 #define USER_LED_2  (47)
 #define SWITCH_2    (29)
+#define USER_LED_TURN_ON  (1)
+#define USER_LED_TURN_OFF (0)
 
 extern void shooting_watch_gpio_create(void);
 extern void shooting_watch_gpio_destroy(void);
@@ -48,10 +50,12 @@ void shooting_process(uint32_t play_time)
   do {
     usleep(500 * 1000);
 
-    shooting_indicator ^= 1;
+    shooting_indicator ^= USER_LED_TURN_ON;
     board_gpio_write(USER_LED_2, shooting_indicator);
 
   } while((time(&cur_time) - start_time) < play_time);
+
+  board_gpio_write(USER_LED_2, USER_LED_TURN_OFF);
 }
 
 
@@ -61,8 +65,6 @@ int main(int argc, FAR char *argv[])
   int game_coundown; 
 
   shooting_watch_gpio_create();
-  board_gpio_write(USER_LED_1, 1);
-  board_gpio_write(USER_LED_2, 1);
 
   printf("Hello, shooting_watch!!!\n");
 
@@ -73,6 +75,9 @@ int main(int argc, FAR char *argv[])
         printf("----- Press SW2 to start the game. When the game starts, shoot SW1 continuously.-----\n");
         printf("----- Press SW1 and SW2 to end the game.-----\n");
         one_time_counter = 1;
+
+        board_gpio_write(USER_LED_1, USER_LED_TURN_OFF);
+        board_gpio_write(USER_LED_2, USER_LED_TURN_OFF);
       }
 
       if (next_state) {
@@ -88,9 +93,11 @@ int main(int argc, FAR char *argv[])
 
     case PRE_SHOOTING:
       for (game_coundown = 3; game_coundown > 0; game_coundown--) {
-        printf("%d...\n", game_coundown);
+        printf("%d...", game_coundown);
+        fflush(stdout);
         sleep(1);
       }
+      printf("\n");
       shooting_count = 0;
       game_state = SHOOTING;
       break;
